c2df78316648f207509799f006b6074fb1f4c409
Omar Saber <amoorzain54@gmail.com>
2025-06-19 05:37:38 +0300

    fix scintificTrack in register

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -7,7 +7,7 @@ import verifyToken from "../services/authService.js";
 const router = express.Router();
 
 const authMiddleware = async (req, res, next) => {
-  const token = authorization.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "No token provided." });
   }
